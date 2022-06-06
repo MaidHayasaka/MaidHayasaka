a4754868b1ae4de43951a031263fc20568dccd50
usbharu <64310155+usbharu@users.noreply.github.com>
2022-06-06 23:03:59 +0900

    change: イベントを MaidHayasakaEventを継承するように変更

---
+++ src/main/kotlin/io/github/usbharu/hayasaka/event/MaidHayasakaEvent.kt
@@ -0,0 +1,5 @@
+package io.github.usbharu.hayasaka.event
+
+import java.util.*
+
+open class MaidHayasakaEvent(source: Any) : EventObject(source)
--- src/main/kotlin/io/github/usbharu/hayasaka/event/ReactionEvent.kt
+++ src/main/kotlin/io/github/usbharu/hayasaka/event/ReactionEvent.kt
@@ -1,7 +1,5 @@
 package io.github.usbharu.hayasaka.event
 
 import io.github.usbharu.hayasaka.model.Reaction
-import java.util.*
 
-class ReactionEvent(source: Any, val reaction: Reaction) : EventObject(source) {
-}
+class ReactionEvent(source: Any, val reaction: Reaction) : MaidHayasakaEvent(source)
