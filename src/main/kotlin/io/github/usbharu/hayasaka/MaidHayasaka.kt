ecf119ce9a6a4a39b3fcdd9a5775db0764fca4cd
usbharu <64310155+usbharu@users.noreply.github.com>
2022-06-13 01:02:16 +0900

    add: slack impl

--- src/main/kotlin/io/github/usbharu/hayasaka/MaidHayasaka.kt
+++ src/main/kotlin/io/github/usbharu/hayasaka/MaidHayasaka.kt
@@ -2,6 +2,7 @@ package io.github.usbharu.hayasaka
 
 import io.github.usbharu.hayasaka.core.EventManager
 import io.github.usbharu.hayasaka.core.service.ServiceFactory
+import io.github.usbharu.hayasaka.core.service.ServiceLoader
 import io.github.usbharu.hayasaka.event.Event
 import io.github.usbharu.hayasaka.event.EventType
 import io.github.usbharu.hayasaka.model.Channel
@@ -9,12 +10,17 @@ import io.github.usbharu.hayasaka.model.Message
 import io.github.usbharu.hayasaka.model.MessageType
 import io.github.usbharu.hayasaka.model.User
 import io.github.usbharu.hayasaka.plugin.loader.PluginLoader
+import org.slf4j.LoggerFactory
+
+val LOGGER = LoggerFactory.getLogger("MaidHayasaka")!!
 
 /**
  * エントリーポイント
  *
  */
 fun main() {
+    LOGGER.info("MaidHayasaka started.")
+    LOGGER.debug(ServiceLoader.SERVICES.toString())
     ServiceFactory.createService().start()
     for (plugin in PluginLoader.PLUGINS) {
         for (listener in plugin.value.plugin.getListeners()) {
